db9204cab4a2f70b11452051bc368751088663bf
Sachin Kumar <sachin.kumar@optym.com>
2019-05-22 12:06:20 +0530

    First Commit

---
+++ Serilogs/SerilogSample/SerilogSample/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Formatting.Json;
+using Serilog.Sinks.Elasticsearch;
+using Serilog.Sinks.File;
+
+namespace SerilogSample
+{
+    class Program
+    {
+        private static IConfiguration Configuration { get; } = new ConfigurationBuilder ()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", true, true)
+            .AddEnvironmentVariables()
+            .Build();
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost:9200")) // for the docker-compose implementation
+                {
+                    AutoRegisterTemplate = true,
+                    //BufferBaseFilename = "./buffer",
+                    RegisterTemplateFailure = RegisterTemplateRecovery.IndexAnyway,
+                    FailureCallback = e => Console.WriteLine("Unable to submit event " + e.MessageTemplate),
+                    EmitEventFailure = EmitEventFailureHandling.WriteToSelfLog |
+                                       EmitEventFailureHandling.WriteToFailureSink |
+                                       EmitEventFailureHandling.RaiseCallback,
+                    FailureSink = new FileSink("./fail-{Date}.txt", new JsonFormatter(), null, null)
+                })
+                .CreateLogger();
+            
+
+            int a = 10, b = 0;
+            try
+            {
+                Log.Debug("Dividing {A} by {B}", a, b);
+                Console.WriteLine(a / b);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Something went wrong");
+            }
+
+            // Introduce a failure by storing a field as a different type
+            Log.Debug("Reusing {A} by {B}", "string", true);
+
+            Log.CloseAndFlush();
+            Console.WriteLine("Press any key to continue...");
+            while (!Console.KeyAvailable)
+            {
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
---
+++ Serilogs/SerilogSample/SerilogSample/SerilogWebHostBuilderExtensions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace SerilogSample
+{
+    public static class SerilogWebHostBuilderExtensions
+    {
+        /// <summary>
+        /// Sets Serilog as the logging provider.
+        /// </summary>
+        /// <param name="builder">The web host builder to configure.</param>
+        /// <param name="logger">The Serilog logger; if not supplied, the static <see cref="Serilog.Log"/> will be used.</param>
+        /// <param name="dispose">When true, dispose <paramref name="logger"/> when the framework disposes the provider. If the
+        /// logger is not specified but <paramref name="dispose"/> is true, the <see cref="Log.CloseAndFlush()"/> method will be
+        /// called on the static <see cref="Log"/> class instead.</param>
+        /// <returns>The web host builder.</returns>
+        public static IWebHostBuilder UseSerilog(this IWebHostBuilder builder, Serilog.ILogger logger = null, bool dispose = false)
+        {
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+            builder.ConfigureServices(collection =>
+                collection.AddSingleton<ILoggerFactory>(services => new SerilogLoggerFactory(logger, dispose)));
+            return builder;
+        }
+
+        /// <summary>Sets Serilog as the logging provider.</summary>
+        /// <remarks>
+        /// A <see cref="WebHostBuilderContext"/> is supplied so that configuration and hosting information can be used.
+        /// The logger will be shut down when application services are disposed.
+        /// </remarks>
+        /// <param name="builder">The web host builder to configure.</param>
+        /// <param name="configureLogger">The delegate for configuring the <see cref="LoggerConfiguration" /> that will be used to construct a <see cref="Logger" />.</param>
+        /// <param name="preserveStaticLogger">Indicates whether to preserve the value of <see cref="Log.Logger"/>.</param>
+        /// <returns>The web host builder.</returns>
+        public static IWebHostBuilder UseSerilog(this IWebHostBuilder builder, Action<WebHostBuilderContext, LoggerConfiguration> configureLogger, bool preserveStaticLogger = false)
+        {
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+            if (configureLogger == null) throw new ArgumentNullException(nameof(configureLogger));
+            builder.ConfigureServices((context, collection) =>
+            {
+                var loggerConfiguration = new LoggerConfiguration();
+                configureLogger(context, loggerConfiguration);
+                var logger = loggerConfiguration.CreateLogger();
+                if (preserveStaticLogger)
+                {
+                    collection.AddSingleton<ILoggerFactory>(services => new SerilogLoggerFactory(logger, true));
+                }
+                else
+                {
+                    // Passing a `null` logger to `SerilogLoggerFactory` results in disposal via
+                    // `Log.CloseAndFlush()`, which additionally replaces the static logger with a no-op.
+                    Log.Logger = logger;
+                    collection.AddSingleton<ILoggerFactory>(services => new SerilogLoggerFactory(null, true));
+                }
+            });
+            return builder;
+        }
+    }
+}
